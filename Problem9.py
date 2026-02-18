f19b39c9f534051e6814fae279cb30cd36a22e36
PriyanshuNSHJ <snh222024@gmail.com>
2026-02-18 23:47:37 +0530

    Update Problem9.py

--- Problem9.py
+++ Problem9.py
@@ -1,8 +1,13 @@
 nums = [2, 7, 11, 15]
 target = 9
 
+seen = {}
+
 for i in range(len(nums)):
-    for j in range(i + 1, len(nums)):
-        if nums[i] + nums[j] == target:
-            print("Indices:", i, j)
-            break
+    complement = target - nums[i]
+    
+    if complement in seen:
+        print("Indices:", seen[complement], i)
+        break
+    
+    seen[nums[i]] = i
